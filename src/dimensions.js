14ea86892deebd691302e97c34e4e873647868b1
gabino <gdabdoub@ezoic.com>
2021-05-11 14:52:03 -0700

    remove dup functions

--- src/dimensions.js
+++ src/dimensions.js
@@ -21,30 +21,6 @@
 		width += parseInt(style.marginLeft) + parseInt(style.marginRight);
 		return width;
 	}
-	$.fn.height = function(val) {
-		if (this.length == 0) {return;}
-		if (val===undefined) {
-			return parseFloat(getComputedStyle(this[0], null).height.replace("px", ""));
-		}
-		if (typeof val === "function") val = val();
-
-		this.each(function(){
-			if (typeof val === "string") this.style.height = val;
-			else this.style.height = val + "px";
-		});
-	}
-	$.fn.width = function(val) {
-		if (this.length == 0) {return;}
-		if (val===undefined) {
-			return parseFloat(getComputedStyle(this[0], null).width.replace("px", ""));
-		}
-		if (typeof val === "function") val = val();
-
-		this.each(function(){
-			if (typeof val === "string") this.style.width = val;
-			else this.style.width = val + "px";
-		});
-	}
   
   })(Zepto)
   
